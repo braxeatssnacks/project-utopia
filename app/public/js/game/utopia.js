e22fe3261e0cf393c721c87661942e66ed3d2b18
Braxton Gunter <braxton.gun@gmail.com>
2017-04-12 03:35:45 -0400

    more game integration progress

--- app/public/js/game/utopia.js
+++ app/public/js/game/utopia.js
@@ -1,8 +1,29 @@
+let width = $('#game').width();
+let height = $('#game').height();
+
 const phaserMethods = { preload: preload, create: create, update: update };
-var game = new Phaser.Game($('#game').width(), $('#game').height(), Phaser.AUTO, 'game', phaserMethods);
+const game = new Phaser.Game(width, height, Phaser.AUTO, 'game', phaserMethods);
 
-function preload() {}
+function preload() {
+  game.load.image('sky', 'img/sprites/sky.png');
+  game.load.image('ground', 'img/sprites/platform.png');
+  game.load.image('star', 'img/sprites/star.png');
+  game.load.spritesheet('dude', 'img/sprites/dude.png', 32, 48);
+}
 
-function create() {}
+function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE); // set physics
+
+  let sky = game.add.tileSprite(0, 0, width, height, 'sky'); // background
+  let platforms = game.add.group(); // platforms
+  let ground = platforms.create(0, game.world.height - 64, 'ground');
+  let player = game.add.sprite(380, game.world.height - 110, 'dude'); // character
+
+  platforms.enableBody = true;
+  ground.scale.setTo(2, 2);
+  ground.body.immovable = true;
+  game.physics.arcade.enable(player);
+  game.camera.follow(player);
+}
 
 function update() {}
